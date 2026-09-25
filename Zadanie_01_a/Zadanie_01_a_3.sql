2fbe065357451bc98d4f27b291129527dab0ad5f
FazulkaBob22 <springtrap555r@gmail.com>
2026-09-25 18:50:54 +0200

    Third exercise

---
+++ Zadanie_01_a/Zadanie_01_a_3.sql
@@ -0,0 +1,6 @@
+SELECT o.order_id, c.customer_name, p.category, o.sales
+FROM orders o
+INNER JOIN customers c ON o.customer_id = c.customer_id
+INNER JOIN products p ON o.product_id = p.product_id
+
+--je tam iba jedna kategoria (furniture), ale v zadani je kategoria a nie sub
